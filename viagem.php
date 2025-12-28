88128d26d74e2c4191064a4c62d4f581f94c0172
martamfilipe <martamartinsfilipe@gmail.com>
2025-12-28 16:32:43 +0000

    css

--- viagem.php
+++ viagem.php
@@ -146,52 +146,49 @@ $_SESSION['last_page'] = 'viagem.php?'
         <section class="galeria-fotos">
             <?php if (!empty($fotos)): ?>
                 <div class="galeria-container">
-                    <?php foreach ($fotos as $index => $foto): ?>
-                        <a href="#foto-<?= $index ?>" class="foto-item">
-                            <img src="<?= htmlspecialchars($foto['path']) ?>" alt="Foto">
-                        </a>
-
-                        <div id="foto-<?= $index ?>" class="modal-sem-js">
-                            <a href="#" class="modal-overlay"></a>
-                            <div class="modal-content">
-                                <a href="#" class="modal-close-btn">&times;</a>
-                                
-                                <?php if ($index > 0): ?>
-                                    <a href="#foto-<?= $index - 1 ?>" class="nav-btn prev">‹</a>
-                                <?php endif; ?>
-
-                                <img src="<?= htmlspecialchars($foto['path']) ?>">
-
-                                <?php if ($index < count($fotos) - 1): ?>
-                                    <a href="#foto-<?= $index + 1 ?>" class="nav-btn next">›</a>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
+                    <div class="galeria-horizontal">
+                        <?php foreach ($fotos as $index => $foto): ?>
+                            <a href="#foto-<?= $index ?>" class="foto-item">
+                                <img src="<?= htmlspecialchars($foto['path']) ?>" alt="Foto da viagem">
+                            </a>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
+                <?php foreach ($fotos as $index => $foto): ?>
+                                <div id="foto-<?= $index ?>" class="modal-sem-js">
+                                    <a href="#" class="modal-overlay"></a>
+                                    <div class="modal-content">
+                                        <a href="#" class="modal-close-btn">&times;</a>
 
-                <?php if ($is_owner && count($fotos) < 16): ?>
-                    <div class="editar-fotos">
-                        <form action="adicionarfotos.php" method="post" enctype="multipart/form-data">
-                            <input type="hidden" name="viagem_id" value="<?= $id_viagem ?>">
-                            <button type="submit">Adicionar Foto</button>
-                        </form>
-                    </div>
-                <?php endif; ?>
+                                        <?php if ($index > 0): ?>
+                                            <a href="#foto-<?= $index - 1 ?>" class="nav-btn prev">‹</a>
+                                        <?php endif; ?>
 
-            <?php else: ?>
+                                        <img src="<?= htmlspecialchars($foto['path']) ?>">
 
-                <?php if ($is_owner): ?>
-                    <div class="editar-fotos">
-                        <form action="adicionarfotos.php" method="post" enctype="multipart/form-data">
-                            <input type="hidden" name="viagem_id" value="<?= $id_viagem ?>">
-                            <button type="submit">Adicionar Foto</button>
-                        </form>
-                    </div>
-                <?php endif; ?>
+                                        <?php if ($index < count($fotos) - 1): ?>
+                                            <a href="#foto-<?= $index + 1 ?>" class="nav-btn next">›</a>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
 
-            <?php endif; ?>
-        </section>
+                            <?php if ($is_owner && count($fotos) < 16): ?>
+                                <form action="adicionarfotos.php" method="post" class="editar-fotos">
+                                    <input type="hidden" name="viagem_id" value="<?= $id_viagem ?>">
+                                    <button type="submit">Adicionar Foto</button>
+                                </form>
+                            <?php endif; ?>
+
+                        <?php elseif ($is_owner): ?>
+
+                            <form action="adicionarfotos.php" method="post" class="editar-fotos">
+                                <input type="hidden" name="viagem_id" value="<?= $id_viagem ?>">
+                                <button type="submit">Adicionar Foto</button>
+                            </form>
+
+                        <?php endif; ?>
+                    </section>
 
 
         <section class="travel-journal">
